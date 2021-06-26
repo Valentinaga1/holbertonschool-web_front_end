f1fd13beeebc3f5e8c876a57ce32d8d88fe7c95f
Valentinaga1 <1881@holbertonschool.com>
2021-06-26 16:08:41 -0500

    First commit

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,17 @@
+function createClassRoom(numbersOfStudents) {
+
+    function studentSeat(seat) {
+        return function () {
+            return seat;
+        }
+    }
+    let students = [];
+    for (i = 0; i < numbersOfStudents; i++) {
+        studentSeat = studentSeat + 1;
+        students.push(studentSeat);
+    }
+
+    return students;
+}
+
+let classRoom = createClassRoom(10);
